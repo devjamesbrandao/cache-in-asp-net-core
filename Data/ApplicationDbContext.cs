b06f3de8b5afb81319787df61a000078b78ee771
James Noleto <muricijames@gmail.com>
2022-08-05 20:36:13 -0300

    feat: DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Caching.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caching.WebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
