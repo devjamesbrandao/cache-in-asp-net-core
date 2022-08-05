6af74d498ea07fc6efdcb20fabd5e27f934d6b44
James Noleto <muricijames@gmail.com>
2022-08-05 20:37:25 -0300

    feat: entity

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Caching.WebApi.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
